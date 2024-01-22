0e01fe977105fc1d35031988d2e2d3b888c6be88
snixf <84739184+pwsh7@users.noreply.github.com>
2024-01-22 15:51:14 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,11 +1,27 @@
-pip install requests
+echo off
+cls
+echo Starting installation...
+pip install requests >nul
+echo installed requests
 pip install discord
 pip uninstall discord
 pip install discord.py==1.7.3
+echo installed discord.py
 pip install winotify
+echo installed winotify
 pip install colorama
+echo installed colorama
 pip install pystyle
+echo installed pystyle
 pip install websocket-client
+echo installed websocket-client
 pip install flask
+echo installed flask
 pip install gevent
+echo installed gevent
 pip install cryptography
+echo installed cryptography
+
+echo Installation done!
+echo have fun using silly selfbot ;)
+pause
